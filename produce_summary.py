2009e68e878ff3944f606374f8a67a97ce861ea3
Josselyn Alvarado <josselynalvarado@Josselyns-MacBook-Air.local>
2022-11-05 20:01:01 -0700

    Initial commit

---
+++ produce_summary.py
@@ -0,0 +1,21 @@
+def going_through_file (day, doc_name):
+    print("Day ", day)
+    the_file = open(doc_name)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+
+going_through_file (1,"um-deliveries-day-1.txt")
+going_through_file (2,"um-deliveries-day-2.txt")
+going_through_file (3,"um-deliveries-day-3.txt")
+
+
+
